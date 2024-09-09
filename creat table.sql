f7d275a84ac80644ad40be973ffc7ebb29cf400f
jeferson-cmd <jeferson.toby@gmail.com>
2024-09-09 19:58:37 -0300

    Add files via upload

---
+++ creat table.sql
@@ -0,0 +1,5 @@
+create table `db_jeferson`.`tbl_nomes` (
+	`id` int not null auto_increment, 
+    `nome` varchar(100) not null,
+    primary key(`id`)
+);
---
+++ insert into.sql
@@ -0,0 +1,2 @@
+insert into `db_jeferson`.`tbl_nomes`
+(`nome`) values ('Pedro');
